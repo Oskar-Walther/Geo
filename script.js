16d90d875790c0081ca8201f5960fb1f305439c3
Oskar Walther <oskarwalther.sueptitz@gmail.com>
2024-12-11 17:40:28 +0100

    first commit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function closeFooter(element){
 }
 
 function toggleFullscreen() {
+    const button = document.querySelector(".fullscreen");
     if (!document.fullscreenElement) {
         // If not in fullscreen mode, request fullscreen
         if (document.documentElement.requestFullscreen) {
@@ -20,6 +21,7 @@ function toggleFullscreen() {
         } else if (document.documentElement.msRequestFullscreen) { // IE11
             document.documentElement.msRequestFullscreen();
         }
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M240-120v-120H120v-80h200v200h-80Zm400 0v-200h200v80H720v120h-80ZM120-640v-80h120v-120h80v200H120Zm520 0v-200h80v120h120v80H640Z"/></svg>';
     } else {
         // If already in fullscreen mode, exit fullscreen
         if (document.exitFullscreen) {
@@ -29,5 +31,6 @@ function toggleFullscreen() {
         } else if (document.msExitFullscreen) { // IE11
             document.msExitFullscreen();
         }
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z"/></svg>'
     }
 }
