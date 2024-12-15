ca36724d6815fd9bd16df599e99f181bbde879d0
Oskar Walther <oskarwalther.sueptitz@gmail.com>
2024-12-15 21:29:24 +0100

    first commit

--- Geoguesser/script.js
+++ Geoguesser/script.js
@@ -5,10 +5,10 @@ const confirm = document.querySelector(".confirm");
 const tasklabel = document.querySelector(".task");
 
 let image = new Image();
-image.src = "/images/location-sign-svgrepo-com.svg";
+image.src = "./images/location-sign-svgrepo-com.svg";
 
 let image2 = new Image();
-image2.src = "/images/my-location-svgrepo-com.svg";
+image2.src = "./images/my-location-svgrepo-com.svg";
 
 var map = L.map("map", {
   center: [52.52, 13.405],
@@ -30,7 +30,7 @@ L.tileLayer(mapLayer, {
 let polygonCoords;
 let center;
 
-getCoords("/data/data.json");
+getCoords("./data/data.json");
 
 async function getCoords(file) {
   let x = await fetch(file);
